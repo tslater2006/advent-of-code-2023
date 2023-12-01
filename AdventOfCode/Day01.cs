ca46a9e67d5e624d13cc85aca861cdb9c21dd99f
Tim Slater <tim.slater@intrasee.com>
2023-11-30 23:36:10 -0600

    Day 1: Initial Solve

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,15 +1,79 @@
-﻿namespace AdventOfCode;
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode;
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
-
+    private readonly string[] _input;
+    string[] WORDS = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadAllLines(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1() {
+        var sum = 0;
+        foreach(var l in _input)
+        {
+            var firstDigit = int.Parse(l.Where(char.IsDigit).First().ToString());
+            var lastDigit = int.Parse(l.Where(char.IsDigit).Last().ToString());
+            sum += firstDigit * 10 + lastDigit;
+        }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        return new(sum.ToString()); 
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var sum = 0;
+        foreach(var l in _input)
+        {
+            var firstDigit = 0;
+            for (var x = 0; x < l.Length; x++)
+            {
+                if (char.IsDigit(l[x]))
+                {
+                    firstDigit = int.Parse(l[x].ToString());
+                    break;
+                }
+                for (var y = 0; y < WORDS.Length; y++)
+                {
+                    if (l.IndexOf(WORDS[y]) == x)
+                    {
+                        firstDigit = y;
+                        break;
+                    }
+                }
+                if (firstDigit != 0)
+                {
+                    break;
+                }
+            }
+
+            var lastDigit = 0;
+            for (var x = l.Length - 1; x >= 0; x--)
+            {
+                if (char.IsDigit(l[x]))
+                {
+                    lastDigit = int.Parse(l[x].ToString());
+                    break;
+                }
+                for (var y = 0; y < WORDS.Length; y++)
+                {
+                    if (l.LastIndexOf(WORDS[y]) == x)
+                    {
+                        lastDigit = y;
+                        break;
+                    }
+                }
+                if (lastDigit != 0)
+                {
+                    break;
+                }
+            }
+            sum += firstDigit * 10 + lastDigit;
+        } 
+            
+        return new(sum.ToString());
+    }
 }
