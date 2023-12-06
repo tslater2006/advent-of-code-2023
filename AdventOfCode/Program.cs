e675f54504616c54dcdcb31b913890119083cce3
Tim Slater <tim.slater@intrasee.com>
2023-12-06 08:47:12 -0600

    Improve AOT output

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -24,6 +24,13 @@ else
 #region AOT
 using AdventOfCode;
 using System.Diagnostics;
+long lastMs = 0;
+void PrintResults(int day, string part1, string part2, long ms)
+{
+    
+    Console.WriteLine($"Day {day} Part 1: {part1} Part 2: {part2} ({ms - lastMs}ms)");
+    lastMs = ms;
+}
 
 Stopwatch sw = new();
 sw.Start();
@@ -33,46 +40,41 @@ var day1 = new AdventOfCode.Day01();
 var day1p1 = await day1.Solve_1();
 var day1p2 = await day1.Solve_2();
 
-Console.WriteLine("Day 1 Part 1: " + day1p1);
-Console.WriteLine("Day 1 Part 2: " + day1p2);
+PrintResults(1, day1p1, day1p2, sw.ElapsedMilliseconds);
 
 var day2 = new Day02();
 
 var day2p1 = await day2.Solve_1();
 var day2p2 = await day2.Solve_2();
 
-Console.WriteLine("Day 2 Part 1: " + day2p1);
-Console.WriteLine("Day 2 Part 2: " + day2p2);
+PrintResults(2, day2p1, day2p2, sw.ElapsedMilliseconds);
 
 var day3 = new Day03();
 
 var day3p1 = await day3.Solve_1();
 var day3p2 = await day3.Solve_2();
 
-Console.WriteLine("Day 3 Part 1: " + day3p1);
-Console.WriteLine("Day 3 Part 2: " + day3p2);
+PrintResults(3, day3p1, day3p2, sw.ElapsedMilliseconds);
 
 var day4 = new AdventOfCode.Day04();
 var day4p1 = await day4.Solve_1();
 var day4p2 = await day4.Solve_2();
 
-Console.WriteLine("Day 4 Part 1: " + day4p1);
-Console.WriteLine("Day 4 Part 2: " + day4p2);
+PrintResults(4, day4p1, day4p2, sw.ElapsedMilliseconds);
 
 var day5 = new AdventOfCode.Day05();
 var day5p1 = await day5.Solve_1();
 var day5p2 = await day5.Solve_2();
 
-Console.WriteLine("Day 5 Part 1: " + day5p1);
-Console.WriteLine("Day 5 Part 2: " + day5p2);
+PrintResults(5, day5p1, day5p2, sw.ElapsedMilliseconds);
 
 var day6 = new AdventOfCode.Day06();
 var day6p1 = await day6.Solve_1();
 var day6p2 = await day6.Solve_2();
 
-Console.WriteLine("Day 6 Part 1: " + day6p1);
-Console.WriteLine("Day 6 Part 2: " + day6p2);
-Console.WriteLine("Done in: " + sw.ElapsedMilliseconds + "ms");
+PrintResults(6, day6p1, day6p2, sw.ElapsedMilliseconds);
+
+Console.WriteLine("\nDone in: " + sw.ElapsedMilliseconds + "ms");
 #endregion
 #endif
 
