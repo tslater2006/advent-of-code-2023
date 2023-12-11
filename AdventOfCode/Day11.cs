ba228877514791d3912d2d9107b24faa667f82b0
Tim Slater <tim.slater@intrasee.com>
2023-12-11 07:40:59 -0600

    Day 11: Part 2 Solution

--- AdventOfCode/Day11.cs
+++ AdventOfCode/Day11.cs
@@ -13,7 +13,6 @@ namespace AdventOfCode
         List<int> EmptyColumns = new();
         List<int> EmptyRows = new();
 
-        
         public Day11()
         {
             int[] GalaxyColumnCounts;
@@ -38,38 +37,43 @@ namespace AdventOfCode
                 if (GalaxyColumnCounts[x] == 0) { EmptyColumns.Add(x); }
             }
         }
-        public override ValueTask<string> Solve_1()
-        {
-            var answer = 0;
 
+        private ulong GetDistanceBetweenGalaxies(ulong expansionRate)
+        {
+            ulong totalDistance = 0;
             /* Loop through every pair of galaxies, order doesn't matter */
-            for(var x = 0; x < Galaxies.Count; x++)
+            for (var x = 0; x < Galaxies.Count; x++)
             {
-                for(var y = x + 1; y < Galaxies.Count; y++)
+                for (var y = x + 1; y < Galaxies.Count; y++)
                 {
                     var galaxy = Galaxies[x];
                     var galaxy2 = Galaxies[y];
 
                     // manhatan distance between the two galaxies
-                    var distance = Math.Abs(galaxy.X - galaxy2.X) + Math.Abs(galaxy.Y - galaxy2.Y);
+                    ulong distance = (ulong)(Math.Abs(galaxy.X - galaxy2.X) + Math.Abs(galaxy.Y - galaxy2.Y));
 
                     /* account for any empty columns between the galaxies */
-                    var emptyColumnsBetween = EmptyColumns.Where(c => c > Math.Min(galaxy.X, galaxy2.X) && c < Math.Max(galaxy.X, galaxy2.X)).Count();
-                    distance += emptyColumnsBetween;
+                    ulong emptyColumnsBetween = (ulong)EmptyColumns.Where(c => c > Math.Min(galaxy.X, galaxy2.X) && c < Math.Max(galaxy.X, galaxy2.X)).Count();
+                    distance += (ulong)(emptyColumnsBetween * (expansionRate - 1));
 
                     /* account for any empty rows between the galaxies */
-                    var emptyRowsBetween = EmptyRows.Where(r => r > Math.Min(galaxy.Y, galaxy2.Y) && r < Math.Max(galaxy.Y, galaxy2.Y)).Count();
-                    distance += emptyRowsBetween;
+                    ulong emptyRowsBetween = (ulong)EmptyRows.Where(r => r > Math.Min(galaxy.Y, galaxy2.Y) && r < Math.Max(galaxy.Y, galaxy2.Y)).Count();
+                    distance += (ulong)(emptyRowsBetween * (expansionRate - 1));
 
-                    answer += distance;
+                    totalDistance += distance;
                 }
             }
-            return new(answer.ToString());
+
+            return totalDistance;
+        }
+        public override ValueTask<string> Solve_1()
+        {
+            return new(GetDistanceBetweenGalaxies(2).ToString());
         }
 
         public override ValueTask<string> Solve_2()
         {
-            throw new NotImplementedException();
+            return new(GetDistanceBetweenGalaxies(1000000).ToString());
         }
     }
 }
